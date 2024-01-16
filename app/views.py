e44ca7a5a70fd8db205e16e8ca0f75d9624c9903
Mert Uçan <mertucan12@gmail.com>
2024-01-16 17:57:57 +0300

    get comments added

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, request, flash
 from app import app, db
 from app.forms import LoginForm, RegisterForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User, Department, Doctors
+from app.models import User, Department, Doctors, Blogs
 
 @app.route('/')
 def index():
@@ -14,7 +14,8 @@ def contact():
 
 @app.route('/blogs')
 def blogs():
-    return render_template('blog-single.html')
+    comments = Blogs.query.all()
+    return render_template('blog-single.html', comments=comments)
 
 @app.route('/portfolio')
 def portfolio():
