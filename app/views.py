d56db6ce9a49d7f5be1a451248972f442e80e835
Mert Uçan <mertucan12@gmail.com>
2024-01-16 23:29:37 +0300

    appointments listed

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, request, flash
 from app import app, db
 from app.forms import LoginForm, RegisterForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User, Department, Doctors, Blogs, BlogTags, Categories
+from app.models import User, Department, Doctors, Blogs, BlogTags, Categories, Appointment
 
 @app.route('/')
 def index():
@@ -11,6 +11,35 @@ def index():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/temp', methods=['GET', 'POST'])
+def temp():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
+    
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('temp'))
+        
+        login_user(user, remember=form.remember_me.data)
+        
+        # Giriş yapan kullanıcının email'ine sahip randevuları çek
+        user_appointments = Appointment.query.filter_by(email=user.email).all()
+        
+        # Randevuları template'e gönder
+        return render_template('dashboard.html', title='Dashboard', appointments=user_appointments)
+    
+    return render_template('temp.html', title='Sign In', form=form)
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    # Giriş yapan kullanıcının email'ine sahip randevuları çek
+    user_appointments = Appointment.query.filter_by(email=current_user.email).all()
+    return render_template('dashboard.html', appointments=user_appointments)
 
 @app.route('/blogs')
 def blogs():
